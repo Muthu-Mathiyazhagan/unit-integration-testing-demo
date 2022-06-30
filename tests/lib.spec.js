119482bbc38a9e0adc00780e88aeb36b8703b288
Muthu <muthu@divum.in>
2022-06-30 12:04:24 +0530

    Grouping the tests and refactor the prod code.

--- tests/lib.spec.js
+++ tests/lib.spec.js
@@ -1,13 +1,17 @@
 const lib = require('../lib');
 
-test(`absolute - It should return positive number if input is positive number `, () => {
-    expect(lib.absolute(1)).toBe(1);
-});
+describe(`absolute`, () => {
 
-test(`absolute - It should return positive number if input is negative number `, () => {
-    expect(lib.absolute(-1)).toBe(1);
-});
+    it(`absolute - It should return positive number if input is positive number `, () => {
+        expect(lib.absolute(1)).toBe(1);
+    });
 
-test(`absolute - It should return zero if input is zero `, () => {
-    expect(lib.absolute(0)).toBe(0);
+    it(`absolute - It should return positive number if input is negative number `, () => {
+        expect(lib.absolute(-1)).toBe(1);
+    });
+
+    it(`absolute - It should return zero if input is zero `, () => {
+        expect(lib.absolute(0)).toBe(0);
+    });
+
 });
